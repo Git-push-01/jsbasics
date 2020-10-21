b693f69eb69a1cbec4706176512ab39586bc5070
Hugo Escandon <hescandon6@gmail.com>
2020-10-21 15:52:05 -0400

    added http-server

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,13 +1,15 @@
-for(var i = 1; i <= 100; i++) {
-let result = ""
-if(i % 3 === 0){
-  result += "fizz"
-
-}if(i % 5 === 0){
-  result += "buzz"
-}if(result== ""){
-  result = i
+for (let i = 1; i <= 100; i++) {
+  let result = "";
+  if (i % 3 === 0) {
+    result += "fizz";
+  }
+  if (i % 5 === 0) {
+    result += "buzz";
+  }
+  if (result == "") {
+    result = i;
+  }
+  console.log(result);
 }
-console.log(result);
-}
-for(let i=0;i<100;)console.log((++i%3?'':'fizz')+(i%5?'':'buzz')||i)
+for (let i = 0; i < 100; )
+  console.log((++i % 3 ? "" : "fizz") + (i % 5 ? "" : "buzz") || i);
