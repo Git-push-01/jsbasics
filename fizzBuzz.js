4a7e2774b4aae72d229d4903b29632bc29ffd2e0
Hugo Escandon <hescandon6@gmail.com>
2020-10-21 14:44:35 -0400

    fizz buzz

---
+++ fizzBuzz.js
@@ -0,0 +1,13 @@
+for(var i = 1; i <= 100; i++) {
+let result = ""
+if(i % 3 === 0){
+  result += "fizz"
+
+}if(i % 5 === 0){
+  result += "buzz"
+}if(result== ""){
+  result = i
+}
+console.log(result);
+}
+for(let i=0;i<100;)console.log((++i%3?'':'fizz')+(i%5?'':'buzz')||i)
